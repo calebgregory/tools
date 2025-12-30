9f449f8b7975f6dbab5c76fe0180f427de976d18
Caleb Gregory <calebgregory@gmail.com>
2025-12-30 11:53:01 -0500

    some refinements to stage and stabilize-720p tasks

--- photo/.mise-tasks/stabilize-720p.py
+++ photo/.mise-tasks/stabilize-720p.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S uv run python
 """
+#MISE description="stabilize and resize videos in a given directory"
+
 ffmpeg + libvidstab takes place in two passes:
     1. analyze motion;
     2. apply transform, given analysis, to stabilize the video.
@@ -11,19 +13,19 @@ libvidstab is fairly featured, and depending on the particulars of the input
 video, you may want to use different options.  maybe at some point i'll play
 with that.  this is at least a decent starting point.
 """
-from functools import partial
-
 import argparse
+import math
 import shutil
 import subprocess
 import sys
 import tempfile
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from pathlib import Path
 import timeit
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from functools import partial
+from pathlib import Path
 
 
-def ffmpeg_analyze_motion(in_: Path, trf: Path, num_ffmpeg_threads: int) -> list[str]:
+def _ffmpeg_analyze_motion(in_: Path, trf: Path, num_ffmpeg_threads: int) -> list[str]:
     return f"""
         ffmpeg -i {in_}
         -threads {num_ffmpeg_threads}
@@ -32,7 +34,7 @@ def ffmpeg_analyze_motion(in_: Path, trf: Path, num_ffmpeg_threads: int) -> list
     """.strip().split()
 
 
-def ffmpeg_stabilize_resize_and_encode(
+def _ffmpeg_stabilize_resize_and_encode(
     in_: Path, trf: Path, out: Path, num_ffmpeg_threads: int
 ) -> list[str]:
     stabilize_expr = f"vidstabtransform=input={trf}:smoothing=14:zoom=1:maxangle=2:maxshift=20"
@@ -49,7 +51,7 @@ def ffmpeg_stabilize_resize_and_encode(
     """.strip().split()
 
 
-def transform_video(out_dir: Path, num_ffmpeg_threads: int, in_: Path) -> tuple[Path, float]:
+def _transform_video(out_dir: Path, num_ffmpeg_threads: int, in_: Path) -> tuple[Path, float]:
     out = out_dir / f"{in_.stem}.720p-stabilized.mp4"
 
     if out.exists():
@@ -63,10 +65,10 @@ def transform_video(out_dir: Path, num_ffmpeg_threads: int, in_: Path) -> tuple[
         tmp_trf.close()  # close the file so ffmpeg can write to it
         trf = Path(tmp_trf.name)
 
-        subprocess.run(ffmpeg_analyze_motion(in_, trf, num_ffmpeg_threads), check=True)
+        subprocess.run(_ffmpeg_analyze_motion(in_, trf, num_ffmpeg_threads), check=True)
 
         subprocess.run(
-            ffmpeg_stabilize_resize_and_encode(in_, trf, out, num_ffmpeg_threads),
+            _ffmpeg_stabilize_resize_and_encode(in_, trf, out, num_ffmpeg_threads),
             check=True,
         )
 
@@ -74,7 +76,20 @@ def transform_video(out_dir: Path, num_ffmpeg_threads: int, in_: Path) -> tuple[
     return out, end - start
 
 
-def main(album_dir: Path, *, num_jobs: int, num_ffmpeg_threads: int) -> None:
+def _derive_parallelization(*, num_files: int) -> tuple[int, int]:
+    assert num_files > 0
+    if num_files <= 2:
+        num_ffmpeg_threads = 0  # let ffmpeg figure out how much CPU it can use
+        return num_files, num_ffmpeg_threads
+
+    # A good heuristic is num_jobs * num_ffmpeg_threads ~= 16–24 for x264 on a big Apple Silicon machine.
+    max_product = 24
+    num_jobs = min(num_files, 8)
+    num_ffmpeg_threads = max(8, math.ceil(max_product / num_jobs))
+    return num_jobs, num_ffmpeg_threads
+
+
+def main(album_dir: Path) -> None:
     if shutil.which("ffmpeg") is None:
         raise EnvironmentError("Error: ffmpeg not found on PATH.")
 
@@ -88,10 +103,11 @@ def main(album_dir: Path, *, num_jobs: int, num_ffmpeg_threads: int) -> None:
     out_dir = album_dir / "720p-stabilized"
     out_dir.mkdir(parents=True, exist_ok=True)
 
-    transform_video_ = partial(transform_video, out_dir, num_ffmpeg_threads)
+    num_jobs, num_ffmpeg_threads = _derive_parallelization(num_files=len(mov_files))
+    transform_video = partial(_transform_video, out_dir, num_ffmpeg_threads)
     start = timeit.default_timer()
-    with ThreadPoolExecutor(max_workers=max(1, num_jobs)) as ex:
-        futures = {ex.submit(transform_video_, mov): mov for mov in mov_files}
+    with ThreadPoolExecutor(max_workers=num_jobs) as ex:
+        futures = {ex.submit(transform_video, mov): mov for mov in mov_files}
 
         try:
             for i, fut in enumerate(as_completed(futures)):
@@ -111,8 +127,6 @@ def main(album_dir: Path, *, num_jobs: int, num_ffmpeg_threads: int) -> None:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("album_dir", type=Path, help="Directory containing .MOV files")
-    parser.add_argument("--jobs", type=int, default=8)
-    parser.add_argument("--ffmpeg-threads", type=int, default=3)
     args = parser.parse_args()
 
-    main(args.album_dir, num_jobs=args.jobs, num_ffmpeg_threads=args.ffmpeg_threads)
+    main(args.album_dir)
